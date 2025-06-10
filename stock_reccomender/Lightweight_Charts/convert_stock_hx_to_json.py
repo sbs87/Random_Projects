fb95a023dc39375fe144f35257a35e1825486e1e
Steve Smith <sbs@stevenbsmith.net>
2025-06-09 20:40:51 -0400

    UPDATE chart to obtain json from user input; clean up json converter script, add a default output filename

--- stock_reccomender/Lightweight_Charts/convert_stock_hx_to_json.py
+++ stock_reccomender/Lightweight_Charts/convert_stock_hx_to_json.py
@@ -5,14 +5,22 @@ import csv
 import json
 from datetime import datetime
 import argparse
+import os
 # Set up argument parsing
-parser = argparse.ArgumentParser(description='Transform stock order data from TSV to JSON format.',usage='python3  convert_stock_hx_to_json.py --input_file /Users/stevensmith/Documents/Other/Financials/Day_trade_stocks/yfinance/stock_order_2025-06-03_MCTR_1m.tsv --output_file stock_order_2025-06-03_MCTR_1m.json')
+parser = argparse.ArgumentParser(description='Transform stock order data from TSV to JSON format.',usage="""python3  convert_stock_hx_to_json.py --input_file /Users/stevensmith/Documents/Other/Financials/Day_trade_stocks/yfinance/stock_order_2025-06-03_MCTR_1m.tsv --output_file stock_order_2025-06-03_MCTR_1m.json
+or (omit output_file to use default name):
+python3  convert_stock_hx_to_json.py --input_file /Users/stevensmith/Documents/Other/Financials/Day_trade_stocks/yfinance/stock_order_2025-06-06_VERO_1m.tsv""")
 parser.add_argument('--input_file', type=str, help='Path to the input TSV file')
-parser.add_argument('--output_file', type=str, help='Path to the output JSON file')
+parser.add_argument('--output_file', type=str, help='Path to the output JSON file. If none is provided, uses input file prefix name (+ local path)',required=False, default=None)
 args = parser.parse_args()  
+#TODO: by default, use basename and prefix for output json. Override with --output_file
 
 input_file = args.input_file
 output_file = args.output_file
+
+if output_file is None:
+    # If no output file is specified, use the input file name with a .json extension
+    output_file = os.path.splitext(os.path.basename(input_file))[0] + '.json'
 
 # Read the TSV file and transform the data
 # Note the time is in Unix seconds which will be correctly read in to Lightweight Charts (e.g., 1 min charts). 
