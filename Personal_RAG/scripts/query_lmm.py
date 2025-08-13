bcc6d559eec7fab2b44f3dd541f9ecc55ceb7f9a
Steve Smith <sbs@stevenbsmith.net>
2025-08-12 21:00:45 -0400

    ADD script to query an LLM, in this case using ollama3/llama3

---
+++ Personal_RAG/scripts/query_lmm.py
@@ -0,0 +1,23 @@
+import requests, json
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Send query to an LLLM.")
+parser.add_argument("--prompt_file",  type=str, help="Input file prompt to send to LLM. Can be RAG-assocaited.")
+args = parser.parse_args()
+prompt_file=args.prompt_file
+
+
+def generate(prompt, provider="openai"):
+    if provider=="openai":
+        r = requests.post("http://localhost:11434/api/generate", json={"model":"llama3","prompt":prompt, "stream":False})
+        return r.json()["response"]
+    
+    #TODO add other providers like local LLMs, etc
+
+if __name__ == "__main__":
+    with open(prompt_file, 'r') as f:
+        prompt = f.read().strip()
+    #prompt = "What is the capital of France?"
+    response = generate(prompt)
+    print(response)
