5b7c378ccb47406c4b67b42feb62a38f0271ae7a
Steve Smith <sbs@stevenbsmith.net>
2025-09-16 20:27:17 -0400

    ADD a Agentic AI chatbot that pretends to be me!

---
+++ agentic_ai/README.md
@@ -0,0 +1,4 @@
+This folder contains some examples playing around with Agentic AI. 
+
+* avatar.ipynb - this is a chatbot that pretends to be me. You can ask questions about me and it will answwer. Based on ChatGPT  (API required). Screen shot of an example of the chat interface:
+![Chatbot Example Screenshot](avatar_chat_example.png)
---
+++ agentic_ai/avatar.py
@@ -0,0 +1,47 @@
+"""
+This creates an avatar for Steve Smith using ChatGPT. 
+Using some information about Steve Smith, it will generate a response to the user's questions.
+
+Sept 2025
+Steve Smith with help from Udemy course on Agentic AI.
+
+"""
+from dotenv import load_dotenv
+from openai import OpenAI
+from pypdf import PdfReader
+import gradio as gr
+load_dotenv(override=True)
+
+
+openai = OpenAI()
+
+reader = PdfReader("me/stevensmith.pdf")
+linkedin = ""
+for page in reader.pages:
+    text = page.extract_text()
+    if text:
+        linkedin += text
+
+with open("me/summary.txt", "r", encoding="utf-8") as f:
+    summary = f.read()
+
+def chat(message, history):
+    messages = [{"role": "system", "content": system_prompt}] + history + [{"role": "user", "content": message}]
+    response = openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
+    return response.choices[0].message.content
+
+
+name = "Steve Smith"
+
+system_prompt = f"You are acting as {name}. You are answering questions on {name}'s website, \
+particularly questions related to {name}'s career, background, skills and experience. \
+Your responsibility is to represent {name} for interactions on the website as faithfully as possible. \
+You are given a summary of {name}'s background and LinkedIn profile which you can use to answer questions. \
+Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
+If you don't know the answer, say so."
+
+system_prompt += f"\n\n## Summary:\n{summary}\n\n## LinkedIn Profile:\n{linkedin}\n\n"
+system_prompt += f"With this context, please chat with the user, always staying in character as {name}."
+
+
+gr.ChatInterface(chat, type="messages").launch()
